b48ecfcb0b5db5f34d598ae898773b08a3cc80f7
Silas <niewierra.silas@gmail.com>
2020-12-28 11:42:31 +0100

    added design updates and start point and pdf logic

--- src/views/painter/Painter.js
+++ src/views/painter/Painter.js
@@ -1,5 +1,9 @@
 import { Component } from "react";
 import './Painter.scss';
+import missing from '../../assets/images/missing.png';
+import { HiDownload } from 'react-icons/hi';
+import { jsPDF } from "jspdf";
+
 
 class Painter extends Component {
 
@@ -10,9 +14,12 @@ class Painter extends Component {
         this.updateInputValue = this.updateInputValue.bind(this);
         this.updateNextPoint = this.updateNextPoint.bind(this);
         this.printList = this.printList.bind(this);
-        this.state = { stepSize: 90, nextPoint: 10, startPoint: 1 };
+        this.setStartingPoint = this.setStartingPoint.bind(this);
+        this.validateStartingPoint = this.validateStartingPoint.bind(this);
+        this.state = { stepSize: 90, nextPoint: 10, startPoint: null, hasBeenDrawn: false };
         this.pointCoordinates = {};
         this.drawingHistory = [];
+        this.tempStart = null;
     }
 
     updateInputValue(evt) {
@@ -23,10 +30,29 @@ class Painter extends Component {
     }
 
     updateNextPoint(evt) {
-        this.setState({
-            nextPoint: evt.target.value
-        });
+        if (evt.target.value <= this.state.stepSize && evt.target.value > 0) {
+            this.setState({
+                nextPoint: evt.target.value
+            });
 
+        } else {
+            alert("Please enter only valid numbers");
+        }
+
+    }
+
+    setStartingPoint() {
+        this.setState({
+            startPoint: this.tempStart
+        });
+    }
+
+    validateStartingPoint(evt) {
+        if (evt.target.value <= this.state.stepSize && evt.target.value > 0) {
+            this.tempStart = evt.target.value;
+        } else {
+            alert("Please enter only valid numbers");
+        }
     }
 
     drawCircle(steps) {
@@ -60,6 +86,9 @@ class Painter extends Component {
 
         }
         ctx.closePath();
+        this.setState({
+            hasBeenDrawn: true
+        })
     }
 
     drawLine(end) {
@@ -92,27 +121,70 @@ class Painter extends Component {
     }
 
     printList() {
-        console.log(this.drawingHistory);
+        var doc = new jsPDF();
+
+        doc.setFontSize(22);
+        doc.text(20, 20, 'Your Custom String Intructions');
+
+        doc.setFontSize(10);
+        doc.text(20, 40, 'Start');
+        var final_index = 0;
+        for (let i = 0; i < this.drawingHistory.length; i++) {
+            doc.text(20, 50 + i * 10, this.drawingHistory[i] + "");
+            final_index = i;
+        }
+        doc.text(20, 50 + (final_index + 1) * 10, 'End');
+
+        doc.save('instructions.pdf');
     }
 
     render() {
         return (
             <div className="painter-wrapper">
                 <div className="painter">
-
                     <canvas id="canvas"></canvas>
-
+                    {!this.state.hasBeenDrawn &&
+                        <div>
+                            <img src={missing} alt=" " width={100} />
+                            Whoops! Seems like you haven't drawn your circle yet
+                        </div>
+                    }
                 </div>
                 <div className="info">
-                    
-                    <input value={this.state.stepSize} onChange={this.updateInputValue} />
-                    <button className="config-button" onClick={() => this.drawCircle(this.state.stepSize)}>Draw The Circle</button>
 
-                    <input value={this.state.nextPoint} onChange={this.updateNextPoint} />
-                    <button className="config-button" onClick={() => this.drawLine(this.state.nextPoint)} >Draw A Line</button>
+                    <h2>String Art Creator</h2>
+                    <p>
+                        Use our intuitive String Art Creator to develop an image consisting of only one string.
+                        Once you're down with the creative part, just print the instructions as pdf and
+                        follow them to create your String Art as a real project.
+                    </p>
 
+                    <h4>Define the amount of circle points</h4>
+                    <div className="config-section-wrapper">
+                        <input className="config-input" value={this.state.stepSize} onChange={this.updateInputValue} type="number" />
+                        <button className="config-button" onClick={() => this.drawCircle(this.state.stepSize)}>Draw The Circle</button>
+                    </div>
+                    {
+                        this.state.hasBeenDrawn && <div>
+                            <h4>Set Your Starting Point</h4>
+                            <div className="config-section-wrapper">
+                                <input className="config-input" value={this.tempStart} onChange={this.validateStartingPoint} type="number" />
+                                <button className="config-button" onClick={this.setStartingPoint} >Set Starting Point</button>
+                            </div>
+                        </div>
+                    }
+                    {
+                        this.state.startPoint !== null &&
+                        <div>
+                            <h4>Draw A New Line</h4>
+                            <div className="config-section-wrapper">
+                                <input className="config-input" value={this.state.nextPoint} onChange={this.updateNextPoint} type="number" />
+                                <button className="config-button" onClick={() => this.drawLine(this.state.nextPoint)} >Draw A Line</button>
+                            </div>
+                        </div>
+                    }
 
-                    <button className="config-button" onClick={() => this.printList()} >Print Drawing History</button>
+                    <button className="download-button" onClick={() => this.printList()} ><HiDownload /> Print Drawing History</button>
                 </div>
             </div>
         );
